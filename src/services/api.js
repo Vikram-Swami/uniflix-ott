69d75d78b0ff5091195b8f81631a5c81f4deb2f9
vikram swami <swamivikram058@gmail.com>
2025-11-28 16:53:38 +0530

    project

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // API service functions
 // Using Vite proxy in development (backend server is optional for video streaming)
 // my website domian https://uniflix-ott.vercel.app
-const BASE_URL = import.meta.env.DEV ? "/api" : "/api";
+const BASE_URL = import.meta.env.DEV ? "/api" : "/api/proxy";
 
 export const fetchHomepage = async () => {
   try {
