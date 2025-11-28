2f1c3ab85aa2d07e873ce54e54ff3c121dd5c012
vikram swami <swamivikram058@gmail.com>
2025-11-28 17:17:27 +0530

    project

--- src/components/usePlaylist.jsx
+++ src/components/usePlaylist.jsx
@@ -55,22 +55,22 @@ const PlaylistProvider = ({ children }) => {
             if (link1.test(text)) {
                 text = text.replace(
                     link1,
-                    'http://localhost:5173/api/media/files/'
+                    'https://uniflix-ott.vercel.app/api/media/files/'
                 );
             } else if (link2.test(text)) {
                 text = text.replace(
                     link2,
-                    'http://localhost:5173/api/media2/files/'
+                    'https://uniflix-ott.vercel.app/api/media2/files/'
                 );
             } else if (link3.test(text)) {
                 text = text.replace(
                     link3,
-                    'http://localhost:5173/api/media3/files/'
+                    'https://uniflix-ott.vercel.app/api/media3/files/'
                 );
             } else if (link4.test(text)) {
                 text = text.replace(
                     link4,
-                    'http://localhost:5173/api/media4/files/'
+                    'https://uniflix-ott.vercel.app/api/media4/files/'
                 );
             } else {
                 console.log("No match found!");
