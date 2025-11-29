0b40c5570840b1747bd1a52a4b8d5d1cf98c46c1
vikram swami <swamivikram058@gmail.com>
2025-11-30 00:55:11 +0530

    project

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -42,7 +42,7 @@ const Watchlist = () => {
   return (
     <div className="min-h-screen bg-black pt-24">
       <div className="max-xs:px-3 xs:px-6 sm2:px-8 md2:px-11 2xl:px-[72px]!">
-        <h1 className="text-base sm:text-lg xl:text-xl font-bold text-white mb-8 md:mb-12">
+        <h1 className="text-base sm:text-xl xl:text-4xl font-bold text-white mb-8 md:mb-12">
           Watchlist
         </h1>
         {watchlistItems.length === 0 ? (
@@ -52,7 +52,7 @@ const Watchlist = () => {
             Add <Link className="underline" to="/movies"> Movies</Link> and <Link className="underline" to="/series"> TV shows</Link> that you want to watch later by clicking Add to Watchlist.
           </p>
         ) : (
-          <div className="grid grid-cols-2 xs:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 3xl:grid-cols-6! lg:gap-3">
+          <div className="grid grid-cols-2 xs:grid-cols-3 md2:grid-cols-4 xl:grid-cols-5 3xl:grid-cols-6! gap-y-3 ms-2.5">
             {watchlistItems.map((item, i) => (
               <MovieCard
                 className="hover:z-50"
