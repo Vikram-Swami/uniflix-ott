dd0f8832561739716071e32e4aaf473ec8b75232
vikram swami <swamivikram058@gmail.com>
2025-11-30 00:40:31 +0530

    project

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -42,7 +42,7 @@ const Watchlist = () => {
   return (
     <div className="min-h-screen bg-black pt-24">
       <div className="px-10">
-        <h1 className="text-[32px] font-bold text-white mb-8 md:mb-12">
+        <h1 className="text-base sm:text-lg xl:text-xl font-bold text-white mb-8 md:mb-12">
           Watchlist
         </h1>
         {watchlistItems.length === 0 ? (
@@ -52,7 +52,7 @@ const Watchlist = () => {
             Add <Link className="underline" to="/movies"> Movies</Link> and <Link className="underline" to="/series"> TV shows</Link> that you want to watch later by clicking Add to Watchlist.
           </p>
         ) : (
-          <div className="grid grid-cols-6 gap-3">
+          <div className="grid grid-cols-5 3xl:grid-cols-6! gap-3">
             {watchlistItems.map((item, i) => (
               <MovieCard
                 className="hover:z-50"
