9cca74ac74820e52ad056e46527b33b908d344f7
vikram swami <swamivikram058@gmail.com>
2025-11-30 01:43:21 +0530

    project

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -40,9 +40,9 @@ const Watchlist = () => {
   }
 
   return (
-    <div className="min-h-screen bg-black pt-24">
-      <div className="px-10">
-        <h1 className="text-[32px] font-bold text-white mb-8 md:mb-12">
+    <div className="min-h-screen bg-black pt-16 xs:pt-20 xl:pt-24">
+      <div className="max-xs:px-3 xs:px-6 sm2:px-8 md2:px-11 2xl:px-[72px]!">
+        <h1 className="text-base md:text-xl lg:text-3xl xl:text-4xl font-bold text-white mb-4 md:mb-8 xl:mb-12">
           Watchlist
         </h1>
         {watchlistItems.length === 0 ? (
@@ -52,7 +52,7 @@ const Watchlist = () => {
             Add <Link className="underline" to="/movies"> Movies</Link> and <Link className="underline" to="/series"> TV shows</Link> that you want to watch later by clicking Add to Watchlist.
           </p>
         ) : (
-          <div className="grid grid-cols-6 gap-3">
+          <div className="grid grid-cols-2 xs:grid-cols-3 md2:grid-cols-4 xl:grid-cols-5 3xl:grid-cols-6! gap-y-3">
             {watchlistItems.map((item, i) => (
               <MovieCard
                 className="hover:z-50"
@@ -64,6 +64,7 @@ const Watchlist = () => {
                 hoveredIndex={hoveredIndex}
                 buttonName="Play"
                 isHovering={isHovering}
+                item2={window.innerWidth > 600 ? false : true}
                 setIsHovering={setIsHovering}
                 handleWatchlist={removeFromWatchlist} />
             ))}
